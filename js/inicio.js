a2e273bc9ce882e0cbfc8ec9d9bb0bff107396b1
Lainix <linkelin8@gmail.com>
2018-06-23 15:57:22 -0500

    Se agrega view y se crea inicio, login, se organizan las paginas

---
+++ js/inicio.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  setTimeout(function(){window.location.href = 'view/login.html';},2000);
+})
+
+// para que lea e inicialice todo
+$(document).ready(function(){
+  });
